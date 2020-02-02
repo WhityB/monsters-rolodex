9055f2a372645e21137107b8e6451d88d3587b8f
Serhii Medvid <mr.medvid@gmail.com>
2020-02-02 20:59:54 +0200

    add build

---
+++ build/precache-manifest.5596219141d58fc718665fc6df7baa2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c2cce310de73f297e595b66a0cfec0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4d4529723a495160be5d",
+    "url": "/monsters-rolodex/static/css/main.cccca0d1.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/monsters-rolodex/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d4529723a495160be5d",
+    "url": "/monsters-rolodex/static/js/main.b8f98b94.chunk.js"
+  },
+  {
+    "revision": "8f564ee1c346f305c76d",
+    "url": "/monsters-rolodex/static/js/runtime-main.17d07582.js"
+  }
+]);
